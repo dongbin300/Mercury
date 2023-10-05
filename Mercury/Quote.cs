c95f5debf587f7d6a7a861dda10b7f206d80a19c
Gaten <dongbin300@gmail.com>
2023-10-06 00:31:32 +0900

    Initial commit

---
+++ Mercury/Quote.cs
@@ -0,0 +1,37 @@
+﻿namespace Mercury
+{
+    public class Quote
+    {
+        public DateTime Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public decimal Volume { get; set; }
+
+        public Quote()
+        {
+
+        }
+
+        public Quote(DateTime date, decimal open, decimal high, decimal low, decimal close, decimal volume)
+        {
+            Date = date;
+            Open = open;
+            High = high;
+            Low = low;
+            Close = close;
+            Volume = volume;
+        }
+
+        public Quote(DateTime date, decimal open, decimal high, decimal low, decimal close)
+        {
+            Date = date;
+            Open = open;
+            High = high;
+            Low = low;
+            Close = close;
+            Volume = 0;
+        }
+    }
+}
